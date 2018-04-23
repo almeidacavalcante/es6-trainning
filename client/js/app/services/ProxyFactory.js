235134de2cbce8ede4a19427ef6b2b8d27760b7f
José de Almeida Cavalcante Neto <jalmeidacn@gmail.com>
2018-04-23 11:25:11 -0300

    improving the performance of the Proxy code.

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -12,6 +12,9 @@ class ProxyFactory {
 
                         console.log(`${prop} INTERCEPTED!`);
                         
+                        // target[prop] is the function on the target object. (add() for example)
+                        // target is the actual object
+                        // arguments is the parameters past to the current method of the target object (add(...parameters) for example)
                         let retorno = Reflect.apply(target[prop], target, arguments);
                         action(target);
                         return retorno;
@@ -21,13 +24,15 @@ class ProxyFactory {
             },
 
             set(target, prop, value, receiver){
+
+                let retorno = Reflect.set(target, prop, value, receiver);
+
                 if(props.includes(prop)){
                     target[prop] = value;
                     action(target);
-                    return true;
                 }
-                Reflect.set(target, prop, value, receiver);
-                return true;
+
+                return retorno;
             }
         })
     }
